4864c29c1729a79331715b15ca05254090b82e5f
Mads Franzen <madsfranzen@live.dk>
2025-03-14 19:23:19 +0100

    todos ready for next session

--- src/main/java/com/worldbuilder/Canvas/Elevation.java
+++ src/main/java/com/worldbuilder/Canvas/Elevation.java
@@ -1,15 +1,18 @@
 package com.worldbuilder.Canvas;
 
 public class Elevation {
-    SandCanvas sandCanvas;
-    GrassCanvas grassCanvas;
-    ShadowCanvas shadowCanvas;
-    StairsCanvas stairsCanvas;
-    WallCanvas wallCanvas;
-    PlateauCanvas plateauCanvas;
-    BridgeCanvas bridgeCanvas;
+    private final SandCanvas sandCanvas;
+    private final GrassCanvas grassCanvas;
+    private final ShadowCanvas shadowCanvas;
+    private final StairsCanvas stairsCanvas;
+    private final WallCanvas wallCanvas;
+    private final PlateauCanvas plateauCanvas;
+    private final BridgeCanvas bridgeCanvas;
 
-    public Elevation(int width, int height) {
+    private final int zIndex;
+
+    public Elevation(int width, int height, int zIndex) {
+        this.zIndex = zIndex;
         this.sandCanvas = new SandCanvas(width, height);
         this.grassCanvas = new GrassCanvas(width, height);
         this.shadowCanvas = new ShadowCanvas(width, height);
@@ -18,4 +21,38 @@ public class Elevation {
         this.plateauCanvas = new PlateauCanvas(width, height);
         this.bridgeCanvas = new BridgeCanvas(width, height);
     }
+
+    public SandCanvas getSandCanvas() {
+        return sandCanvas;
+    }
+
+    public GrassCanvas getGrassCanvas() {
+        return grassCanvas;
+    }
+
+    public ShadowCanvas getShadowCanvas() {
+        return shadowCanvas;
+    }
+
+    public StairsCanvas getStairsCanvas() {
+        return stairsCanvas;
+    }
+
+    public WallCanvas getWallCanvas() {
+        return wallCanvas;
+    }
+
+    public PlateauCanvas getPlateauCanvas() {
+        return plateauCanvas;
+    }
+
+    public BridgeCanvas getBridgeCanvas() {
+        return bridgeCanvas;
+    }
+
+    public int getZIndex() {
+        return zIndex;
+    }
+
+ 
 }
--- src/main/java/com/worldbuilder/Canvas/StairsCanvas.java
+++ src/main/java/com/worldbuilder/Canvas/StairsCanvas.java
@@ -2,10 +2,13 @@ package com.worldbuilder.Canvas;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 public class StairsCanvas extends Canvas {
     private final GraphicsContext gc;
     private final StairsTile[][] tileMap;
+    private final Image TILESET = new Image(
+            GroundCanvas.class.getResourceAsStream("/assets/Terrain/Ground/Tilemap_Elevation.png"));
 
     public StairsCanvas(int width, int height) {
         super(width, height);
@@ -13,16 +16,28 @@ public class StairsCanvas extends Canvas {
         this.tileMap = new StairsTile[width][height];
     }
 
-    private static record StairsTile() {}
+    private static record StairsTile() {
+    }
 
-    private static record TileVariant(String name, int x, int y) {}
+    private static record TileVariant(String name, int x, int y) {
+    }
 
     private static final TileVariant[] VARIANTS = {
-        new TileVariant("TOP_LEFT", 0, 0),
-        new TileVariant("TOP", 1, 0),
-        new TileVariant("TOP_RIGHT", 2, 0),
-        new TileVariant("LEFT", 0, 1),
-        new TileVariant("CENTER", 1, 1),
+            new TileVariant("LEFT", 0, 1),
+            new TileVariant("CENTER", 1, 1),
+            new TileVariant("RIGHT", 2, 1),
+            new TileVariant("SOLO", 0, 0)
     };
-    
+
+    public void drawStairs(int x, int y, TileVariant variant) {
+        // TODO: STEAL FROM GroundCanvas
+    }
+
+    public void deleteStairs(int x, int y) {
+        // TODO: STEAL FROM GroundCanvas
+    }
+
+    public void updateNeighbors(int x, int y) {
+        // TODO: STEAL FROM GroundCanvas
+    }
 }
